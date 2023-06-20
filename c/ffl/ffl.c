fd1ed8d5691a1124a11a99f57c9c3ff646237c36
xb <ssbandjl@163.com>
2023-06-21 00:24:32 +0800

    ffl_time

--- c/ffl/ffl.c
+++ c/ffl/ffl.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 // #define INFO(msg, ...) \
 //     fprintf(stderr, "info: %s:%d: ", __FILE__, __LINE__); \
@@ -17,6 +18,7 @@
 // #endif
 
 
+
 #ifdef WIN32
 #define TrimFilePath(x) strrchr(x,'\\')?strrchr(x,'\\')+1:x
 #else //*nix
@@ -24,10 +26,26 @@
 #endif
  
 #define dprint(fmt, ...)   \
-printf("[DEBUG]  " fmt" %s:%d\n", ##__VA_ARGS__, TrimFilePath(__FILE__) ,__LINE__)
+    time_t timer; \
+    char buffer[26]; \
+    struct tm* tm_info; \
+    timer = time(NULL); \
+    tm_info = localtime(&timer); \
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info); \
+    printf("[%s] "fmt" %s %s:%d\n", buffer, ##__VA_ARGS__, __FUNCTION__, TrimFilePath(__FILE__) ,__LINE__)
 
 // gcc -o main ffl.c;./main
 int main() {
+    // time_t timer;
+    // char buffer[26];
+    // struct tm* tm_info;
+
+    // timer = time(NULL);
+    // tm_info = localtime(&timer);
+
+    // strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    // puts(buffer);
+
     char a[120] = "jkdajgld";
     // printf("%s, %s\n", __FFL__, __FILENAME__);
     // printf("%#x\n", a);
