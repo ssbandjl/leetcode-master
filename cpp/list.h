f8a978466713f3858fd587dbfc627690fefe7a86
xb <ssbandjl@163.com>
2023-12-01 04:09:02 +0800

    sort link

---
+++ cpp/list.h
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+// 单链表
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+
+class MyLinkedList {
+public:
+    MyLinkedList() {
+        this->size = 0;
+        this->head = new ListNode(0);
+    }
+    
+    int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode *cur = head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur->next;
+        }
+        return cur->val;
+    }
+    int get_len() {
+        int len = 0;
+        ListNode *cur = head;
+        while (cur != NULL){
+            cur = cur->next;
+            len++;
+        }
+        return len;
+    }
+    
+    void for_each(){
+        ListNode *cur = head;
+        while (cur != NULL) {
+            cout << cur->val << endl;
+            cur = cur->next;
+        }
+    }
+    
+    void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+    
+    void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+    
+    void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        index = max(0, index);
+        size++;
+        ListNode *pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred->next;
+        }
+        ListNode *toAdd = new ListNode(val);
+        toAdd->next = pred->next;
+        pred->next = toAdd;
+    }
+    
+    void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+        size--;
+        ListNode *pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred->next;
+        }
+        ListNode *p = pred->next;
+        pred->next = pred->next->next;
+        delete p;
+    }
+// private:
+    int size;
+    ListNode *head;
+};
+
+
---
+++ cpp/main.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include "./list.h"
+using namespace std;
+
+
+/* 
+148. 排序链表: https://leetcode.cn/problems/sort-list/description/
+链表排序, 分治和归并 
+*/
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        return sortList(head, nullptr);
+    }
+
+    ListNode* sortList(ListNode* head, ListNode* tail) {
+        if (head == nullptr) {
+            return head;
+        }
+        if (head->next == tail) {
+            head->next = nullptr;
+            return head;
+        }
+        ListNode* slow = head, *fast = head;
+        while (fast != tail) {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast != tail) {
+                fast = fast->next; // fast point move two times than slow
+            }
+        }
+        ListNode* mid = slow;
+        return merge(sortList(head, mid), sortList(mid, tail));
+    }
+
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* temp = dummyHead, *temp1 = head1, *temp2 = head2;
+        while (temp1 != nullptr && temp2 != nullptr) {
+            if (temp1->val <= temp2->val) {
+                temp->next = temp1;
+                temp1 = temp1->next;
+            } else {
+                temp->next = temp2;
+                temp2 = temp2->next;
+            }
+            temp = temp->next;
+        }
+        if (temp1 != nullptr) {
+            temp->next = temp1;
+        } else if (temp2 != nullptr) {
+            temp->next = temp2;
+        }
+        return dummyHead->next;
+    }
+};
+
+// g++ -o main soft_link.cpp && ./main
+int main() {
+    /**
+     * Your MyLinkedList object will be instantiated and called as such:
+     * MyLinkedList* obj = new MyLinkedList();
+     * int param_1 = obj->get(index);
+     * obj->addAtHead(val);
+     * obj->addAtTail(val);
+     * obj->addAtIndex(index,val);
+     * obj->deleteAtIndex(index);
+     */
+    MyLinkedList* obj = new MyLinkedList();
+    obj->addAtTail(1);
+    obj->addAtTail(5);
+    obj->addAtTail(2);
+    obj->addAtTail(8);
+    obj->for_each();
+    Solution solution;
+    cout << "solution_res:" << solution.sortList(obj->head) << endl;
+    obj->for_each();
+}
