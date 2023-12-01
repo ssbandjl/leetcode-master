1a30ce8f3c62d50525909626540136baab6bf742
xb <ssbandjl@163.com>
2023-12-02 05:10:29 +0800

    update

---
+++ cpp/merge_two_link_for_range.cc
@@ -0,0 +1,54 @@
+//  https://leetcode.cn/problems/merge-two-sorted-lists/solutions/
+
+/* 
+方法二：迭代
+思路
+
+我们可以用迭代的方法来实现上述算法。当 l1 和 l2 都不是空链表时，判断 l1 和 l2 哪一个链表的头节点的值更小，将较小值的节点添加到结果里，当一个节点被添加到结果里之后，将对应链表中的节点向后移一位。
+
+算法
+
+首先，我们设定一个哨兵节点 prehead ，这可以在最后让我们比较容易地返回合并后的链表。我们维护一个 prev 指针，我们需要做的是调整它的 next 指针。然后，我们重复以下过程，直到 l1 或者 l2 指向了 null ：如果 l1 当前节点的值小于等于 l2 ，我们就把 l1 当前的节点接在 prev 节点的后面同时将 l1 指针往后移一位。否则，我们对 l2 做同样的操作。不管我们将哪一个元素接在了后面，我们都需要把 prev 向后移一位。
+
+在循环终止的时候， l1 和 l2 至多有一个是非空的。由于输入的两个链表都是有序的，所以不管哪个链表是非空的，它包含的所有元素都比前面已经合并链表中的所有元素都要大。这意味着我们只需要简单地将非空链表接在合并链表的后面，并返回合并链表即可。
+
+下图展示了 1->4->5 和 1->2->3->6 两个链表迭代合并的过程
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/merge-two-sorted-lists/solutions/226408/he-bing-liang-ge-you-xu-lian-biao-by-leetcode-solu/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ * 
+ * 
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *preHead = new ListNode(-1);
+        ListNode *pre = preHead;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                pre->next = list1;
+                list1 = list1->next;
+            } else {
+                pre->next = list2;
+                list2 = list2->next;
+            }
+            pre = pre->next;
+        }
+        pre->next = list1 == nullptr ? list2 : list1;
+        return preHead->next;
+    }
+};
