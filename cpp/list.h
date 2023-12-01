f89d70a72293674fcff1f8433b15bb18949976cb
xb <ssbandjl@163.com>
2023-12-01 15:27:29 +0800

    update

--- cpp/list.h
+++ cpp/list.h
@@ -7,7 +7,9 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 
